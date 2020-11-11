9e69daaabd8c7c2a243fe3606d31be01ac0ee6be
cep104 <candicelizabethpeters@gmail.com>
2020-11-10 21:38:42 -0600

    commit tree 5547aad06f899689d86aa46c1650bb50c240cc89

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,16 @@
 # Code your solution here!
+require 'pry'
+def run_guessing_game
+    number = rand(6)+1
+    puts "Guess a number between 1 and 6:"
+    input = gets.strip
+    # binding.pry
+    if input == number.to_s #input is a string
+      puts  "You guessed the correct number!"
+    elsif input == "exit"
+        puts "Goodbye!"
+    else 
+        puts "Sorry! The computer guessed #{number}."
+    end
+    
+end
